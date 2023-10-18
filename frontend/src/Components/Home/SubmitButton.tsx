f641176fd726492eda584d1242dd34bc37dab883
cavej <ozangencay123@gmail.com>
2023-10-18 23:53:37 +0300

    submit button added

---
+++ frontend/src/Components/Home/SubmitButton.tsx
@@ -0,0 +1,11 @@
+const SubmitButton = () => {
+  return (
+    <div className="mt-20 w-[85%] flex flex-col items-end ">
+      <div className="w-[40%] flex flex-col items-center">
+        <button className="w-[75%] h-11 bg-blue-600 rounded-3xl drop-shadow-md hover:drop-shadow-xl"></button>
+      </div>
+    </div>
+  )
+}
+
+export default SubmitButton
