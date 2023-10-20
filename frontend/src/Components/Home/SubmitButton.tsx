1f627c18beca6963b7f20ae1a381f4b1e08821b8
cavej <ozangencay123@gmail.com>
2023-10-20 19:22:05 +0300

    placeholder text added

--- frontend/src/Components/Home/SubmitButton.tsx
+++ frontend/src/Components/Home/SubmitButton.tsx
@@ -4,7 +4,7 @@ const SubmitButton = () => {
       <div className="w-1/2 flex flex-col items-center">
         <button className="w-[80%] h-11 bg-blue-600 rounded-3xl drop-shadow-md hover:drop-shadow-xl"></button>
       </div>
-      <div className="mt-1 text-white w-1/2 flex flex-col items-center font-roboto-slab text-center">
+      <div className="mt-1 text-white w-1/2 flex flex-col items-center font-roboto-slab text-center md:text-base text-xs">
         <p>
           Beni cefadan bıktıran yâr cefa etmekten <a className="text-blue-500 hover:text-blue-700" href="https://meral.club">bıkmaz</a> mı?
         </p>
