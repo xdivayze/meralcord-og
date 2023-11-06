99fdf8048bd63a17c103c4ac09985b1fa22ede9f
cavej <ozangencay123@gmail.com>
2023-11-06 20:31:54 +0300

    backend initialization

--- frontend/src/Components/Home/ObeseBar.tsx
+++ frontend/src/Components/Home/ObeseBar.tsx
@@ -7,7 +7,8 @@ enum TextfieldTypes {
 const ObeseBar = ({ text, type }: { text: string, type: TextfieldTypes }) => {
   return (
     <div className="mt-6 min-h-[10vh] w-full md:w-[50%] bg-dark-clay rounded-3xl drop-shadow-xl flex justify-center ">
-      <input placeholder={text} className="text-left w-[90%] font-roboto-slab md:text-xl text-sm px-4 py-2 rounded-full bg-gray-300 text-blue-700 focus:outline-none " type={type} />
+      <input placeholder={text} className="text-left w-[90%] font-roboto-slab md:text-xl text-sm px-4 py-2 rounded-l-3xl rounded-r-3xl bg-gray-300 text-blue-700 
+        outline-none border-cyan-500 border-x-8 " type={type} />
 
     </div>
   )
