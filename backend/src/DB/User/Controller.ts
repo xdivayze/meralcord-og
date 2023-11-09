1a8070165a2241622e5c5e9f3594c224c5787112
cavej <ozangencay123@gmail.com>
2023-11-09 21:41:28 +0300

    added login options

--- backend/src/DB/User/Controller.ts
+++ backend/src/DB/User/Controller.ts
@@ -1,16 +1,18 @@
 import * as express from 'express'
 import { IUser, UserModel } from './Model'
+import * as crypto from 'crypto'
+import { DEFAULT_EXPIRATION_TIME } from '../constants'
 
 const router = express.Router()
 
-router.post("/register", async(req: express.Request, res:express.Response) => {
+router.post("/register", async (req: express.Request, res: express.Response) => {
   const body: IUser = req.body
   console.log(body)
-  const foundUser = await UserModel.findOne({email:body.email})
+  const foundUser = await UserModel.findOne({ email: body.email }).exec()
   if (foundUser) {
-    res.status(400).send({error: "user with same e mail exists"})
+    res.status(400).send({ error: "user with same e mail exists" })
     return
-  } 
+  }
 
   try {
     const createUser = async (payload: IUser) => {
@@ -18,10 +20,54 @@ router.post("/register", async(req: express.Request, res:express.Response) => {
       await mod.save()
     }
     await createUser(req.body)
-    res.status(202).send({success: true})
+    res.status(202).send({ success: true })
+    return
   } catch (err) {
-    res.status(400).send({error: err})
+    res.status(400).send({ error: err })
+    return
   }
 })
 
-export {router}
+router.get("/login", async (req: express.Request, res: express.Response) => {
+  const email = req.headers.email
+  const passwdHash = req.headers.passwordhash
+  console.log(req.headers)
+  if (!email || !passwdHash) {
+    res.status(400).send(
+      { "error": `required headers are not included ${JSON.stringify({ email, passwdHash })}` }
+    )
+    return
+  }
+
+  const foundUser = await UserModel.findOne({ email }).exec()
+  if (!foundUser) {
+    res.status(400).send({ "error": "user not found" })
+    return
+  }
+  const passwdHashFound = foundUser.get("passwordHash")
+  if (!passwdHashFound) {
+    res.status(400).send({ "error": "password hash was not found on found user" })
+    return
+  }
+  if (passwdHashFound !== passwdHash) {
+    res.status(400).send({ "error": "passwords don't match" })
+    return
+  } else {
+    const authCode = crypto.randomBytes(16).toString('hex')
+    foundUser.authorizationCodes.push({
+      authCode,
+      expiration: Math.floor(Date.now() / 1000) + DEFAULT_EXPIRATION_TIME
+    })
+    foundUser.save()
+    res.status(202).send({
+      "success": true,
+      "authorizationCode": authCode
+    })
+    return
+  }
+
+
+
+})
+
+export { router }
--- backend/src/DB/User/Model.ts
+++ backend/src/DB/User/Model.ts
@@ -1,17 +1,24 @@
 import { Document, Schema, model } from "mongoose"
 
+interface IAuthorization {
+  expiration: number,
+  authCode: string
+}
+
 interface IUser extends Document {
   email: string,
   passwordHash: string,
-  rsapub: string
+  rsapub: string,
+  authorizationCodes: Array<IAuthorization>
 }
 
 const UserSchema = new Schema({
   passwordHash: { type: String, required: true },
   email: { type: String, required: true },
-  rsapub: {type: String, required: true}
+  rsapub: { type: String, required: true },
+  authorizationCodes: { type: Array<IAuthorization>, required: false }
 })
 
 const UserModel = model<IUser>("User", UserSchema)
 
-export {UserModel, IUser}
+export { UserModel, IUser }
