70767471eccced3d7d1870b571699ae5bd9461df
cavej <ozangencay123@gmail.com>
2023-11-12 08:41:31 +0300

    lol

--- frontend/src/Components/Home/ObeseBar.tsx
+++ frontend/src/Components/Home/ObeseBar.tsx
@@ -1,14 +1,20 @@
+import { SetStateAction, Dispatch } from "react"
+
 enum TextfieldTypes {
   password = "password",
   text = "text"
 }
 
 
-const ObeseBar = ({ text, type }: { text: string, type: TextfieldTypes }) => {
+const ObeseBar = ({ text, type, setState }: { text: string, type: TextfieldTypes, setState: Dispatch<SetStateAction<string>> | null }) => {
   return (
     <div className="mt-6 min-h-[10vh] w-full md:w-full bg-dark-clay rounded-3xl drop-shadow-xl flex justify-center ">
-      <input placeholder={text} className="text-left w-[90%] font-roboto-slab md:text-xl text-sm px-4 py-2 rounded-l-3xl rounded-r-3xl bg-gray-300 text-blue-700 
-        outline-none border-cyan-500 border-x-8 " type={type} />
+      <input onChange={(state) => { 
+        if (setState !== null ) {
+          setState(state.target.value)
+        } 
+       }} placeholder={text} className="text-left w-[90%] font-roboto-slab md:text-xl text-sm px-4 py-2 rounded-l-3xl rounded-r-3xl bg-gray-300 text-blue-700
+      outline-none border-cyan-500 border-x-8 " type={type} />
 
     </div>
   )
--- frontend/src/Components/Register/Register.tsx
+++ frontend/src/Components/Register/Register.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import LongBar from "../Home/LongBar"
 import { ObeseBar, TextfieldTypes } from "../Home/ObeseBar"
 import { SubmitButton } from "../Home/SubmitButton"
@@ -12,7 +13,16 @@ const TextSpace = () => {
   )
 }
 
+const doRegister = (regParams: { mail: string, passwd: string, cdv: string, seed: string }) => {
+
+}
+
 const Register = () => {
+  const [mail, setMail] = useState("")
+  const [passwd, setPasswd] = useState("")
+  const [cdv, setCDV] = useState("")
+  const [seed, setSeed] = useState("")
+
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
       <div className="w-full mt-14 md:mt-28 items-center flex justify-center">
@@ -21,21 +31,20 @@ const Register = () => {
       <div className="flex-col md:flex-row flex w-[85%] mt-16">
         <div className="md:w-1/3 w-full h-full items-center flex flex-col order-1">
           <div className="w-full items-end flex flex-col">
-            <ObeseBar text="meral@meral.club" type={TextfieldTypes.text} />
-            <ObeseBar text="********" type={TextfieldTypes.password} />
-            <ObeseBar text="code de vérification" type={TextfieldTypes.text} />
+            <ObeseBar setState={setMail} text="meral@meral.club" type={TextfieldTypes.text} />
+            <ObeseBar setState={setPasswd} text="********" type={TextfieldTypes.password} />
+            <ObeseBar setState={setCDV} text="code de vérification" type={TextfieldTypes.text} />
           </div>
         </div>
         <div className="md:w-1/3 w-full h-full items-center flex flex-col md:order-2 order-3 ">
           <TextSpace />
-
           <SubmitButton text="Kayıt" />
         </div>
         <div className="md:w-1/3 w-full h-full items-center flex flex-col md:order-3 order-2">
           <div className="w-full items-end flex flex-col">
-            <ObeseBar text="seed" type={TextfieldTypes.text} />
-            <ObeseBar text="rsapriv" type={TextfieldTypes.password} />
-            <ObeseBar text="rsapub" type={TextfieldTypes.text} />
+            <ObeseBar setState={setSeed} text="seed" type={TextfieldTypes.text} />
+            <ObeseBar setState={null} text="rsapriv" type={TextfieldTypes.password} />
+            <ObeseBar setState={null} text="rsapub" type={TextfieldTypes.text} />
           </div>
         </div>
 
